0a3059154d9b408ecfa5d7649b435c5ec99f6e17
Sean Heffernan <seanmheff@gmail.com>
2018-03-31 00:45:15 +0000

    Make something

--- lib/torrentleech.ex
+++ lib/torrentleech.ex
@@ -1,18 +1,67 @@
 defmodule Torrentleech do
-  @moduledoc """
-  Documentation for Torrentleech.
-  """
+  use Agent
 
-  @doc """
-  Hello world.
+  @login_url "https://www.torrentleech.org/user/account/login/"
+  @search_url "https://www.torrentleech.org/torrents/browse/autocomplete"
 
-  ## Examples
+  def start_link do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
 
-      iex> Torrentleech.hello
-      :world
+  def get(key) do
+    Agent.get(__MODULE__, &Map.get(&1, key))
+  end
 
-  """
-  def hello do
-    :world
+  def put(key, value) do
+    Agent.update(__MODULE__, &Map.put(&1, key, value))
+  end
+
+  def login(username, password) do
+    body = {:form, [username: username, password: password]}
+    headers = %{"Content-type" => "application/x-www-form-urlencoded"}
+    cookies = HTTPoison.post!(@login_url, body, headers).headers
+              |> Enum.filter(fn
+                {"Set-Cookie", _} -> true
+                _ -> false
+              end)
+              |> Enum.map(fn(tuple) -> elem(tuple, 1) end)
+              |> Enum.map(fn(item) -> List.first(String.split(item, " ")) end)
+              |> Enum.map(fn(cookie) -> String.split(cookie, "=", parts: 2) end)
+              |> Enum.reduce(%{}, fn(item, acc) ->
+                [key | value] = item
+                Map.put(acc, key, value)
+              end)
+              |> Enum.map(fn{k,v} -> "#{k}=#{v}" end)
+              |> Enum.join(" ")
+    put("cookies", cookies)
+  end
+
+  def search(query) do
+    options = [
+      params: [search: query, excludeMusicCheck: "", categoryID: "28"]
+    ]
+    headers = %{
+      "Accept" => "application/json",
+      "Content-type" => "application/x-www-form-urlencoded; charset=UTF-8",
+      "Cookie" => get("cookies"),
+      "x-requested-with" => "XMLHttpRequest"
+    }
+    HTTPoison.get!(@search_url, headers, options).body
+  end
+
+  def download(url) do
+    headers = %{ "Cookie" => get("cookies") }
+    resp = HTTPoison.get!(url, headers)
+    tmp = resp.headers
+          |> List.keyfind("Content-Disposition", 0)
+          |> elem(1)
+          |> String.split
+          |> List.last
+    filename = Regex.named_captures(~r/filename=\"(?<filename>.*)\"/, tmp)["filename"]
+    File.write!("/tmp/#{filename}", resp.body)
+  end
+
+  def is_logged_in? do
+    get("cookies") != nil;
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,8 +4,7 @@ defmodule Torrentleech.MixProject do
   def project do
     [
       app: :torrentleech,
-      version: "0.1.0",
-      elixir: "~> 1.6",
+      version: "0.1.0", elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +20,9 @@ defmodule Torrentleech.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 1.0"},
+      {:floki, "~> 0.20.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
